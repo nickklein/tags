794dcb2e416c184d4dc13ccea05b656799e1463c
Nick Klein <1294247+nickklein@users.noreply.github.com>
2025-02-02 09:27:19 -0800

    add resource

---
+++ src/Resources/TagsResource.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace NickKlein\Tags\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TagsResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return $this->tag->tag_name;
+    }
+}
