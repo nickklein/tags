f38bd5fd32a9dd1607b23094d6e4c8b6cc799c72
Nick Klein <1294247+nickklein@users.noreply.github.com>
2025-02-02 12:03:42 -0800

    exclude habits stuff

--- src/Services/TagsService.php
+++ src/Services/TagsService.php
@@ -33,26 +33,4 @@ class TagsService
 
         return ['action' => 'success'];
     }
-
-    // destroy tag for habit times
-    public function destroyHabitTimesTag(int $userId, int $habitTimeId, string $tagName, LogsService $log): array
-    {
-        try {
-            $tag = $this->repository->findTag($tagName);
-            if (empty($tag)) {
-                return [];
-            }
-
-            $habitTimesTag = $this->repository->findHabitTimesTag($habitTimeId, $tag->tag_id, $userId);
-            if (!$habitTimesTag->delete()) {
-                return [];
-            }
-
-            return ['action' => 'success'];
-        } catch (Exception $e) {
-            // Log the exception using the LogsService
-            $log->handle("Error", "Exception occurred in destroyHabitTimesTag: " . $e->getMessage());
-            return ['action' => 'error', 'message' => 'An error occurred while processing the request.'];
-        }
-    }
 }
